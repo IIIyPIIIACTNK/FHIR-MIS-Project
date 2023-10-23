9fc4f5202b1665acadf42f3486b680a61347d97b
Алексей Шустров <geremusb@mail.ru>
2023-10-23 21:57:26 +0300

    Basic set up

---
+++ FHIR-MIS-webview/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Hl7.Fhir.Model;
+
+namespace FHIR_MIS_webview
+{
+  public class ApplicationDBContext : DbContext
+  {
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+    {
+    }
+    DbSet<Patient> Patinets;
+
+
+  }
+}
---
+++ FHIR-MIS-webview/FireLyServerConnectionTESTING.cs
@@ -0,0 +1,7 @@
+﻿namespace FHIR_MIS_webview
+{
+  public class FireLyServerConnectionTESTING
+  {
+    private readonly string _firelyUrl = "";
+  }
+}
